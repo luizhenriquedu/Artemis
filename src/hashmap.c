333e6457be82e2d90e1ff2a16323f611a7be4b60
Luiz Henrique <luizhen428@gmail.com>
2024-12-01 18:02:05 -0300

    feat(hashmap): added hashmap_hash function

--- src/hashmap.c
+++ src/hashmap.c
@@ -1,1 +1,15 @@
 #include <include/hashmap.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+uint64_t hashmap_hash(char *key, size_t size)
+{
+    int hash_value = 0;
+    while (*key)
+    {
+        hash_value = (hash_value << 5) + *key;
+        key++;
+    }
+    return hash_value % size;
+}
--- src/include/hashmap.h
+++ src/include/hashmap.h
@@ -20,3 +20,11 @@ typedef struct
     hm_table_t *table;
     uint64_t flags;
 } hashmap_t;
+
+hashmap_t *hashmap_create(size_t size, size_t collisions);
+uint64_t hashmap_hash(const char *key, size_t size);
+void *hashmap_get(hashmap_t *hm, char *key);
+void hashmap_resize(hashmap_t *hm, uint64_t new_size, uint64_t max_collisions);
+void hashmap_import(hashmap_t *to, hashmap_t *from);
+void hashmap_destroy(hashmap_t *hm);
+void hashmap_destroy_free_items(hashmap_t *hm);
