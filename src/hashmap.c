97537119e29f278a1a6ce5aca216cec1cafcb827
Luiz Henrique <luizhen428@gmail.com>
2024-12-01 22:12:02 -0300

    feat(hashmap): added hashmap add entry function

--- src/hashmap.c
+++ src/hashmap.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
-uint64_t hashmap_hash(char *key, size_t size)
+uint64_t hashmap_hash(const char *key, size_t size)
 {
     int hash_value = 0;
     while (*key)
@@ -11,7 +12,7 @@ uint64_t hashmap_hash(char *key, size_t size)
         hash_value = (hash_value << 5) + *key;
         key++;
     }
-    return hash_value % size;
+    return hash_value % (size - 1);
 }
 
 hashmap_t *hashmap_create(size_t size, size_t max_collisions)
@@ -20,4 +21,31 @@ hashmap_t *hashmap_create(size_t size, size_t max_collisions)
     map->size = size;
     map->max_collisions = max_collisions;
     return map;
+}
+
+void hashmap_add(hashmap_t *hashmap, char *key, void *data)
+{
+start:
+{
+    uint64_t index = hash(key, hashmap->size);
+
+    hashmap_table_t *table = &hashmap->table[index];
+    if (table->entries == NULL || table->collisions == 0)
+    {
+        table->entries = (hashmap_entry_t *)malloc(sizeof(hashmap_entry_t) * hashmap->max_collisions);
+        memset(table->entries, 0, sizeof(hashmap_entry_t) * hashmap->max_collisions);
+        table->collisions = 0;
+    }
+
+    if (table->collisions == hashmap->max_collisions)
+    {
+
+        goto start;
+    }
+
+    uint64_t idx = table->collisions++;
+    table->entries[idx].key = (char *)malloc(strlen(key));
+    strcpy(table->entries[idx].key, key);
+    table->entries[idx].data = data;
+}
 }
--- src/include/hashmap.h
+++ src/include/hashmap.h
@@ -10,21 +10,22 @@ typedef struct
 typedef struct
 {
     hashmap_entry_t *entries;
-    size_t collision;
-} hm_table_t;
+    size_t collisions;
+} hashmap_table_t;
 
 typedef struct
 {
     size_t size;
     size_t max_collisions;
-    hm_table_t *table;
+    hashmap_table_t *table;
     uint64_t flags;
 } hashmap_t;
 
 hashmap_t *hashmap_create(size_t size, size_t collisions);
 uint64_t hashmap_hash(const char *key, size_t size);
-void *hashmap_get(hashmap_t *hm, char *key);
-void hashmap_resize(hashmap_t *hm, uint64_t new_size, uint64_t max_collisions);
+void hashmap_add(hashmap_t *hashmap, char *key, void *data);
+void *hashmap_get(hashmap_t *hashmap, char *key);
+void hashmap_resize(hashmap_t *hashmap, uint64_t new_size, uint64_t max_collisions);
 void hashmap_import(hashmap_t *to, hashmap_t *from);
-void hashmap_destroy(hashmap_t *hm);
-void hashmap_destroy_free_items(hashmap_t *hm);
+void hashmap_destroy(hashmap_t *hashmap);
+void hashmap_destroy_free_items(hashmap_t *hashmap);
